2fbcf92155badd410c20b27b121460b8e9f6db1e
Eric Rochester <erochest@gmail.com>
2022-11-25 17:28:05 -0600

    added todos and planning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
+// TODO: read from a file and dump out syntax
+// TODO: parse an integer (`42`)
+// TODO: read from stdin
+// TODO: parse a symbol (`foobar`)
+// TODO: parse a float (`3.14159`)
+// TODO: parse a rational number (`2/3`)
+// TODO: parse a string (`"string with spaces"`)
+// TODO: parse nil (`nil`)
+// TODO: parse a cons list (`(42 43 44)`)
+// TODO: parse a dotted-cons cell (`(42 . 43)`)
+// TODO: parse a quoted symbol (`'foobar`)
+// TODO: parse a quoted list (`'(+ 1 3)`)
+// TODO: parse a quoted function name (`#'foobar`)
+// TODO: parse comments
+
 fn main() {
     println!("Hello, world!");
 }
