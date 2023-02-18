9cdede1b0955034c195a0f229fdd0643c22945ce
Eric Rochester <erochest@gmail.com>
2023-02-18 14:05:12 -0500

    input parameter

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -7,3 +7,12 @@ fn test_main() {
     let output = cmd.unwrap();
     assert!(output.status.success());
 }
+
+#[test]
+fn test_empty_file() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let cmd = cmd.args(&["--input".to_string(), "tests/data/empty.fth".to_string()]);
+    let output = cmd.unwrap();
+    assert!(output.status.success());
+}
+
