6780779bbb75fa8d2f21ce582a89dc24da1223f2
Eric Rochester <erochest@gmail.com>
2023-02-21 07:39:29 -0500

    Moved todos to the right module

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,6 @@ use human_panic::setup_panic;
 use lisp::error::Result;
 use lisp::reader::read_lisp;
 
-// TODO: parse a rational number (`2/3`)
-// TODO: parse a string (`"string with spaces"`)
-// TODO: parse nil (`nil`)
-// TODO: parse an empty list (`()`)
-// TODO: parse a dotted-cons cell (`(42 . 43)`)
-// TODO: parse a cons list (`(42 43 44)`)
-// TODO: parse a quoted symbol (`'foobar`)
-// TODO: parse a quoted list (`'(+ 1 3)`)
-// TODO: parse a quoted function name (`#'foobar`)
-// TODO: parse comments
-
 fn main() -> Result<()> {
     setup_panic!();
     let args = Cli::parse();
--- src/reader.rs
+++ src/reader.rs
@@ -4,6 +4,17 @@ use serde::Serialize;
 
 use crate::error::Result;
 
+// TODO: parse a rational number (`2/3`)
+// TODO: parse a string (`"string with spaces"`)
+// TODO: parse nil (`nil`)
+// TODO: parse an empty list (`()`)
+// TODO: parse a dotted-cons cell (`(42 . 43)`)
+// TODO: parse a cons list (`(42 43 44)`)
+// TODO: parse a quoted symbol (`'foobar`)
+// TODO: parse a quoted list (`'(+ 1 3)`)
+// TODO: parse a quoted function name (`#'foobar`)
+// TODO: parse comments
+
 #[derive(Debug, Serialize)]
 #[serde(tag = "type")]
 pub enum Token {
