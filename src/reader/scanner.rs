02e3e61e3e57f908e98df29f9051708a0de9dd12
Eric Rochester <erochest@gmail.com>
2023-02-22 20:22:09 -0500

    Skips initial whitespace

--- src/reader/scanner.rs
+++ src/reader/scanner.rs
@@ -4,24 +4,31 @@ use crate::error::Result;
 
 struct ScanRanges {
     i: usize,
-    buffer: String,
+    buffer: Vec<char>,
 }
 
 impl Iterator for ScanRanges {
     type Item = Range<usize>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.i < self.buffer.len() {
-            self.i = self.buffer.len();
-            Some(0..self.buffer.len())
-        } else {
-            None
+
+        while self.buffer.get(self.i).map_or(false, |c| c.is_whitespace()) {
+            self.i += 1;
         }
+
+        if self.i >= self.buffer.len() {
+            return None
+        }
+
+        let start = self.i;
+        self.i = self.buffer.len();
+
+        Some(start..self.buffer.len())
     }
 }
 
-fn scan<R: Read>(reader: R) -> Result<impl Iterator<Item = Range<usize>>> {
-    let buffer = read_to_string(reader)?;
+fn scan<R: Read>(reader: R) -> Result<ScanRanges> {
+    let buffer = read_to_string(reader)?.chars().collect::<Vec<_>>();
     Ok(ScanRanges {
         i: 0,
         buffer,
@@ -31,6 +38,8 @@ fn scan<R: Read>(reader: R) -> Result<impl Iterator<Item = Range<usize>>> {
 #[cfg(test)]
 mod tests {
     use pretty_assertions::assert_eq;
+
+    use crate::reader::scanner::ScanRanges;
 
     use super::scan;
 
@@ -54,5 +63,23 @@ mod tests {
 
         let ranges = result.unwrap().collect::<Vec<_>>();
         assert_eq!(ranges.len(), 1);
+    }
+
+    // TODO: only whitespace
+
+    #[test]
+    fn test_skips_initial_whitespace() {
+        let mut input = "    foobar".as_bytes();
+
+        let result = scan(&mut input);
+        assert!(result.is_ok());
+
+        let scanner: ScanRanges = result.unwrap();
+        let buffer = scanner.buffer.clone();
+        let ranges = scanner.collect::<Vec<_>>();
+        assert_eq!(ranges.len(), 1);
+
+        let token = buffer.get(ranges[0].clone()).map(|cs| cs.iter().collect::<String>());
+        assert_eq!(token, Some("foobar".to_string()));
     }
 }
