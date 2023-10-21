2c3b800c6b1a4e4eae24dd672ba19b385abb153b
Eric Rochester <erochest@gmail.com>
2023-10-21 15:48:21 -0400

    Token::is_cons

--- src/token.rs
+++ src/token.rs
@@ -15,3 +15,30 @@ pub enum Token {
     Nil,
     Comment,
 }
+
+impl Token {
+    pub fn is_cons(&self) -> bool {
+        matches!(self, Token::Cons { .. })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_token() {
+        let token = Token::Integer { value: 1 };
+        assert_eq!(token, Token::Integer { value: 1 });
+    }
+
+    // Test a new method called `is_cons` that returns true if the token is a cons cell.
+    #[test]
+    fn test_is_cons() {
+        let token = Token::Cons {
+            head: Box::new(Token::Integer { value: 1 }),
+            tail: Box::new(Token::Integer { value: 2 }),
+        };
+        assert_eq!(token.is_cons(), true);
+    }
+}
